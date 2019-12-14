bd0162388fcb0f3ff083099f492b40e504b388e8
moddipen@gmail.com <moddipen@gmail.com>
2019-12-14 14:35:07 +0530

    setup packages

--- frontend/src/services/RequestService.js
+++ frontend/src/services/RequestService.js
@@ -5,7 +5,7 @@ delete axios.defaults.headers.common["Accept"];
 export const getRequest = api => {
   return new Promise((resolve, reject) => {
     return axios
-      .get(apiHost + api)
+      .get(process.env.API_HOST + api)
       .then(resp => {
         resolve(resp);
       })
@@ -18,7 +18,7 @@ export const getRequest = api => {
 export const postRequest = (api, data = {}, header = {}) => {
   return new Promise((resolve, reject) => {
     return axios
-      .post(apiHost + api, data, header)
+      .post(process.env.API_HOST + api, data, header)
       .then(resp => {
         resolve(resp);
       })
@@ -31,7 +31,7 @@ export const postRequest = (api, data = {}, header = {}) => {
 export const putRequest = (api, data = {}) => {
   return new Promise((resolve, reject) => {
     axios
-      .put(apiHost + api, data)
+      .put(process.env.API_HOST + api, data)
       .then(resp => {
         resolve(resp);
       })
@@ -44,7 +44,7 @@ export const putRequest = (api, data = {}) => {
 export const deleteRequest = api => {
   return new Promise((resolve, reject) => {
     axios
-      .delete(apiHost + api)
+      .delete(process.env.API_HOST + api)
       .then(resp => {
         resolve(resp);
       })
